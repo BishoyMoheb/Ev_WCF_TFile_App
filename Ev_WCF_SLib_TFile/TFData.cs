373ae706f4da27fbe4399ed4a6856464868bae5f
Bishoy Moheb <bishoy_hnn@yahoo.co.uk>
2021-12-06 14:20:24 +0200

    Text File reading and doing operation on it

---
+++ Ev_WCF_SLib_TFile/TFData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev_WCF_SLib_TFile
+{
+    [DataContract]
+    public class TFData
+    {
+        public int EmpID { get; set; }
+        public int ProjectID { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
